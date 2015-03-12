ee43bbfd2db9765ee1233f689ec527a969a2dc37
Evert Pot <me@evertpot.com>
2015-03-12 18:33:38 -0400

    Docblocks

--- lib/NotFoundException.php
+++ lib/NotFoundException.php
@@ -4,4 +4,12 @@ namespace Sabre\ResourceLocator;
 
 use Exception;
 
-class NotFoundException extends Exception { }
+/**
+ * Emitted when a resource could not be found.
+ *
+ * @copyright Copyright (C) 2015 fruux GmbH. (https://fruux.com/)
+ * @author Evert Pot (http://evertpot.com/)
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class NotFoundException extends Exception {
+}
